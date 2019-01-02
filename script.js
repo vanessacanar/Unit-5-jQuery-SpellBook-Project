76b0dcf821c8a2cfddbf7057503656c74fdeea76
vanessacanar <44434875+vanessacanar@users.noreply.github.com>
2019-01-02 08:30:56 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#magic").click(function(){
+ $("#trick1").fadeOut();
+});
+
+
+
+//Spell #2
+
+$("#hat").click(function(){
+$("#bunny").fadeIn();
+});
+
+
+
+//Spell #3
+
+$("#card").click(function(){
+$("#gone").slideUp(); 
+});
+
+
+
